d87ca3c2f751a1bc21a9afe97aef5db78edc75af
HIRAM E JIMENEZ <jimenez.hiram@gmail.com>
2017-01-21 11:29:06 -0500

    linking screens

--- app/src/main/java/com/sparx1126/steamworks/BenchmarkTeleopSwitcher.java
+++ app/src/main/java/com/sparx1126/steamworks/BenchmarkTeleopSwitcher.java
@@ -1,19 +1,37 @@
 package com.sparx1126.steamworks;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class BenchmarkTeleopSwitcher extends AppCompatActivity {
 
+    Button benchmarkAutoSwitcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.benchmarkteleop);
 
+        benchmarkAutoSwitcher = (Button)findViewById(R.id.benchmarkAutoSwitcher);
+        benchmarkAutoSwitcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                benchmarkAutoSwitcherButtonClicked(v);
+            }
+        });
+    }
+
+    private void benchmarkAutoSwitcherButtonClicked(View v) {
+        Context context = BenchmarkTeleopSwitcher.this;
+        Class destination = BenchmarkAuto.class;
+        Intent intent = new Intent(context, destination);
+        startActivity(intent);
     }
 
 }
