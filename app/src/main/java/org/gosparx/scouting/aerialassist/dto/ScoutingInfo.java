d8a90910aa3ee9676547099daec7734cfa6b3d13
Amanda <arlonghenry@gmail.com>
2017-01-28 16:45:27 -0500

    Minor Changes

--- app/src/main/java/org/gosparx/scouting/aerialassist/dto/ScoutingInfo.java
+++ app/src/main/java/org/gosparx/scouting/aerialassist/dto/ScoutingInfo.java
@@ -8,74 +8,142 @@ public class ScoutingInfo {
     private String nameOfScouter;
     private String eventKey;
     private String teamKey;
-
-    //drives
-
+    private boolean canPickUpBallsFromHopper;
+    private boolean canPickUpBallsFromGround;
+    private boolean canPickUpGearsFromGround;
+    private boolean canPickUpGearsFromRetrieval;
+    private String preferredGearRetrieval;
+    private int ballCapacity;
+    private boolean canShootHigh;
+    private String typeOfShooter;
+    private int ballsPerSecond;
+    private String shootingLocation;
+    private String preferredShootingLocation;
+    private boolean canShootLow;
+    private int lowGoalRating;
+    private boolean abilityToScale;
+    private String placesCanScaleFrom;
+    private String preferredScalePlace;
     private String driveSystemDescription;
     private double approxSpeedFeetPerSecond = 0.0;
-    // which defenses the robot can cross
-    private Boolean canCrossPortcullis = false;
-    private Boolean canCrossCheval = false;
-    private Boolean canCrossMoat = false;
-    private Boolean canCrossRamparts = false;
-    private Boolean canCrossDrawbridge = false;
-    private Boolean canCrossSallyport = false;
-    private Boolean canCrossRockwall = false;
-    private Boolean canCrossRoughterrain = false;
-    private Boolean canCrossLowbar = false;
-    //does the robot extend beyond the transport config?
-    //TODO get clarification on this...
-    private Boolean doesExtendBeyondTransportConfig = false;
 
-    //auto
-
-    // starting position in autonomous
-    private Boolean autoStartInNeutralZone = false;
-    private Boolean autoStartInSpyPosition = false;
-    //ending position in autonomous
-    private Boolean autoEndInCourtyard = false;
-
-
-    private Boolean autoEndInNeutralZone = false;
-    // what the robot can do in autonomous
-    private String autoCapabilitiesDescription;
-    // where the robot can acquire boulders
-
-    //acquisition
-
-    private Boolean acquiresBouldersFromFloor = false;
-    private Boolean acquiresBouldersFromHumanPlayer = false;
-    // boulder acquisition preference: "human" or "floor"
-    private String preferredBoulderSource = "unknown";
-    // whether or not boulders can be carried over defenses
-    private Boolean canCarryBouldersOverPortcullis = false;
-    private Boolean canCarryBouldersOverCheval = false;
-    private Boolean canCarryBouldersOverMoat = false;
-    private Boolean canCarryBouldersOverRamparts = false;
-    private Boolean canCarryBouldersOverDrawbridge = false;
-    private Boolean canCarryBouldersOverSallyport = false;
-    private Boolean canCarryBouldersOverRockwall = false;
-    private Boolean canCarryBouldersOverRoughterrain = false;
-    private Boolean canCarryBouldersOverLowbar = false;
-
-    //scoring
-
-    // whether or not goals can be scored
     private Boolean canScoreInHighGoal = false;
     private Boolean canScoreInLowGoal = false;
-    private double averageHighGoalsPerMatch = 0.0;
-    private double averageLowGoalsPerMatch = 0.0;
 
-    // scaling ability
-    private Boolean canScaleAtCenter = false;
-    private Boolean canScaleOnRight = false;
-    private Boolean canScaleOnLeft = false;
-    private double scaleHeightPercent = 0.0;
-    //cycle time
-    private double cycleTime = 0.0;
-    //defense
-    private Boolean playsDefense = false;
+    public String preferredGearRetrieval() {
+        return preferredGearRetrieval;
+    }
 
+    public void setPreferredGearRetrieval(String preferredGearRetrieval) {
+        this.preferredGearRetrieval = preferredGearRetrieval;
+    }
+
+    public String getPreferredShootingLocation() {
+        return preferredShootingLocation;
+    }
+
+    public void setPreferredShootingLocation(String preferredShootingLocation) {
+        this.preferredShootingLocation = preferredShootingLocation; }
+
+
+    public String preferredScalePLace() {
+        return preferredScalePlace;
+    }
+
+    public void preferredScalePlace(String preferredScalePlace) {
+        this.preferredScalePlace = preferredScalePlace;
+    }
+    
+
+    public String placesCanScaleFrom() {
+        return placesCanScaleFrom;
+    }
+
+    public void placesCanScaleFrom(String placesCanScaleFrom) {
+        this.placesCanScaleFrom = placesCanScaleFrom;
+    }
+
+
+    public void canShootLow(Boolean canShootLow) { this.canShootLow = canShootHigh(); }
+
+    public Boolean canShootLow() {
+        return canShootLow;
+    }
+
+    public int getLowGoalRating() {
+        return lowGoalRating;
+    }
+
+    public void setLowGoalRating(int lowGoalRating) { this.lowGoalRating = ballsPerSecond(); }
+
+    public void abilityToScale(Boolean abilityToScale) { this.abilityToScale = canShootHigh(); }
+
+    public Boolean abilityToScale() {
+        return abilityToScale;
+    }
+
+
+
+    public String shootingLocation() {
+        return shootingLocation;
+    }
+
+    public void ShootingLocation(String shootingLocation) {
+        this.shootingLocation = shootingLocation; }
+
+
+    public int ballsPerSecond() {
+        return ballsPerSecond;
+    }
+
+    public void ballsPerSecond(Boolean ballsPerSecond) { this.ballsPerSecond = ballsPerSecond(); }
+
+    public String typeOfShooter() {
+        return typeOfShooter;
+    }
+
+    public void typeOfShooter(String typeOfShooter) {
+        this.typeOfShooter = typeOfShooter;
+    }
+
+
+    public void canShootHigh(Boolean canShootHigh) { this.canShootHigh = canShootHigh(); }
+
+    public Boolean canShootHigh() {
+        return canShootHigh;
+    }
+
+
+
+    public int ballCapacity() {
+        return ballCapacity;
+    }
+
+    public void ballCapacity(Boolean canPickUpGearsFromRetrieval) { this.ballCapacity = ballCapacity(); }
+
+    public Boolean canPickUpGearsFromRetrieval() {
+        return canPickUpGearsFromRetrieval;
+    }
+
+    public void setCanPickUpGearsFromRetrieval(Boolean canPickUpGearsFromRetrieval) { this.canPickUpGearsFromRetrieval = canPickUpGearsFromRetrieval; }
+
+    public Boolean canPickUpGearsFromGround() {
+        return canPickUpGearsFromGround;
+    }
+
+    public void canPickUpGearsFromGround(Boolean canPickUpGearsFromGround) { this.canPickUpGearsFromGround = canPickUpGearsFromGround; }
+
+    public Boolean canPickUpBallsFromGround() {
+        return canPickUpBallsFromGround;
+    }
+
+    public void canPickUpBallsFromGround(Boolean canPickUpBallsFromGround) { this.canPickUpBallsFromGround = canPickUpBallsFromGround; }
+
+    public Boolean canPickUpBallsFromHopper() {
+        return canPickUpBallsFromHopper;
+    }
+
+    public void canPickUpBallsFromHopper(Boolean canPickUpBallsFromHopper) { this.canPickUpBallsFromHopper = canPickUpBallsFromHopper; }
 
     public String getNameOfScouter() {
         return nameOfScouter;
@@ -117,205 +185,7 @@ public class ScoutingInfo {
         this.approxSpeedFeetPerSecond = approxSpeedFeetPerSecond;
     }
 
-    public Boolean getCanCrossPortcullis() {
-        return canCrossPortcullis;
-    }
 
-    public void setCanCrossPortcullis(Boolean canCrossPortcullis) {
-        this.canCrossPortcullis = canCrossPortcullis;
-    }
-
-    public Boolean getCanCrossCheval() {
-        return canCrossCheval;
-    }
-
-    public void setCanCrossCheval(Boolean canCrossCheval) {
-        this.canCrossCheval = canCrossCheval;
-    }
-
-    public Boolean getCanCrossMoat() {
-        return canCrossMoat;
-    }
-
-    public void setCanCrossMoat(Boolean canCrossMoat) {
-        this.canCrossMoat = canCrossMoat;
-    }
-
-    public Boolean getCanCrossRamparts() {
-        return canCrossRamparts;
-    }
-
-    public void setCanCrossRamparts(Boolean canCrossRamparts) {
-        this.canCrossRamparts = canCrossRamparts;
-    }
-
-    public Boolean getCanCrossDrawbridge() {
-        return canCrossDrawbridge;
-    }
-
-    public void setCanCrossDrawbridge(Boolean canCrossDrawbridge) {
-        this.canCrossDrawbridge = canCrossDrawbridge;
-    }
-
-    public Boolean getCanCrossSallyport() {
-        return canCrossSallyport;
-    }
-
-    public void setCanCrossSallyport(Boolean canCrossSallyport) {
-        this.canCrossSallyport = canCrossSallyport;
-    }
-
-    public Boolean getCanCrossRockwall() {
-        return canCrossRockwall;
-    }
-
-    public void setCanCrossRockwall(Boolean canCrossRockwall) {
-        this.canCrossRockwall = canCrossRockwall;
-    }
-
-    public Boolean getCanCrossRoughterrain() {
-        return canCrossRoughterrain;
-    }
-
-    public void setCanCrossRoughterrain(Boolean canCrossRoughterrain) {
-        this.canCrossRoughterrain = canCrossRoughterrain;
-    }
-
-    public Boolean getCanCrossLowbar() {
-        return canCrossLowbar;
-    }
-
-    public void setCanCrossLowbar(Boolean canCrossLowbar) {
-        this.canCrossLowbar = canCrossLowbar;
-    }
-
-    public Boolean getDoesExtendBeyondTransportConfig() {
-        return doesExtendBeyondTransportConfig;
-    }
-
-    public void setDoesExtendBeyondTransportConfig(Boolean doesExtendBeyondTransportConfig) {
-        this.doesExtendBeyondTransportConfig = doesExtendBeyondTransportConfig;
-    }
-
-    public Boolean getAutoStartInNeutralZone() {
-        return autoStartInNeutralZone;
-    }
-
-    public void setAutoStartInNeutralZone(Boolean autoStartInNeutralZone) {
-        this.autoStartInNeutralZone = autoStartInNeutralZone;
-    }
-
-    public Boolean getAutoStartInSpyPosition() {
-        return autoStartInSpyPosition;
-    }
-
-    public void setAutoStartInSpyPosition(Boolean autoStartInSpyPosition) {
-        this.autoStartInSpyPosition = autoStartInSpyPosition;
-    }
-
-    public String getAutoCapabilitiesDescription() {
-        return autoCapabilitiesDescription;
-    }
-
-    public void setAutoCapabilitiesDescription(String autoCapabilitiesDescription) {
-        this.autoCapabilitiesDescription = autoCapabilitiesDescription;
-    }
-
-    public Boolean getAcquiresBouldersFromFloor() {
-        return acquiresBouldersFromFloor;
-    }
-
-    public void setAcquiresBouldersFromFloor(Boolean acquiresBouldersFromFloor) {
-        this.acquiresBouldersFromFloor = acquiresBouldersFromFloor;
-    }
-
-    public Boolean getAcquiresBouldersFromHumanPlayer() {
-        return acquiresBouldersFromHumanPlayer;
-    }
-
-    public void setAcquiresBouldersFromHumanPlayer(Boolean acquiresBouldersFromHumanPlayer) {
-        this.acquiresBouldersFromHumanPlayer = acquiresBouldersFromHumanPlayer;
-    }
-
-    public String getPreferredBoulderSource() {
-        return preferredBoulderSource;
-    }
-
-    public void setPreferredBoulderSource(String preferredBoulderSource) {
-        this.preferredBoulderSource = preferredBoulderSource;
-    }
-
-    public Boolean getCanCarryBouldersOverPortcullis() {
-        return canCarryBouldersOverPortcullis;
-    }
-
-    public void setCanCarryBouldersOverPortcullis(Boolean canCarryBouldersOverPortcullis) {
-        this.canCarryBouldersOverPortcullis = canCarryBouldersOverPortcullis;
-    }
-
-    public Boolean getCanCarryBouldersOverCheval() {
-        return canCarryBouldersOverCheval;
-    }
-
-    public void setCanCarryBouldersOverCheval(Boolean canCarryBouldersOverCheval) {
-        this.canCarryBouldersOverCheval = canCarryBouldersOverCheval;
-    }
-
-    public Boolean getCanCarryBouldersOverMoat() {
-        return canCarryBouldersOverMoat;
-    }
-
-    public void setCanCarryBouldersOverMoat(Boolean canCarryBouldersOverMoat) {
-        this.canCarryBouldersOverMoat = canCarryBouldersOverMoat;
-    }
-
-    public Boolean getCanCarryBouldersOverRamparts() {
-        return canCarryBouldersOverRamparts;
-    }
-
-    public void setCanCarryBouldersOverRamparts(Boolean canCarryBouldersOverRamparts) {
-        this.canCarryBouldersOverRamparts = canCarryBouldersOverRamparts;
-    }
-
-    public Boolean getCanCarryBouldersOverDrawbridge() {
-        return canCarryBouldersOverDrawbridge;
-    }
-
-    public void setCanCarryBouldersOverDrawbridge(Boolean canCarryBouldersOverDrawbridge) {
-        this.canCarryBouldersOverDrawbridge = canCarryBouldersOverDrawbridge;
-    }
-
-    public Boolean getCanCarryBouldersOverSallyport() {
-        return canCarryBouldersOverSallyport;
-    }
-
-    public void setCanCarryBouldersOverSallyport(Boolean canCarryBouldersOverSallyport) {
-        this.canCarryBouldersOverSallyport = canCarryBouldersOverSallyport;
-    }
-
-    public Boolean getCanCarryBouldersOverRockwall() {
-        return canCarryBouldersOverRockwall;
-    }
-
-    public void setCanCarryBouldersOverRockwall(Boolean canCarryBouldersOverRockwall) {
-        this.canCarryBouldersOverRockwall = canCarryBouldersOverRockwall;
-    }
-
-    public Boolean getCanCarryBouldersOverRoughterrain() {
-        return canCarryBouldersOverRoughterrain;
-    }
-
-    public void setCanCarryBouldersOverRoughterrain(Boolean canCarryBouldersOverRoughterrain) {
-        this.canCarryBouldersOverRoughterrain = canCarryBouldersOverRoughterrain;
-    }
-
-    public Boolean getCanCarryBouldersOverLowbar() {
-        return canCarryBouldersOverLowbar;
-    }
-
-    public void setCanCarryBouldersOverLowbar(Boolean canCarryBouldersOverLowbar) {
-        this.canCarryBouldersOverLowbar = canCarryBouldersOverLowbar;
-    }
 
     public Boolean getCanScoreInHighGoal() {
         return canScoreInHighGoal;
@@ -331,87 +201,6 @@ public class ScoutingInfo {
 
     public void setCanScoreInLowGoal(Boolean canScoreInLowGoal) {
         this.canScoreInLowGoal = canScoreInLowGoal;
-    }
-
-    public double getAverageHighGoalsPerMatch() {
-        return averageHighGoalsPerMatch;
-    }
-
-    public void setAverageHighGoalsPerMatch(double averageHighGoalsPerMatch) {
-        this.averageHighGoalsPerMatch = averageHighGoalsPerMatch;
-    }
-
-    public double getAverageLowGoalsPerMatch() {
-        return averageLowGoalsPerMatch;
-    }
-
-    public void setAverageLowGoalsPerMatch(double averageLowGoalsPerMatch) {
-        this.averageLowGoalsPerMatch = averageLowGoalsPerMatch;
-    }
-
-    public double getCycleTime() {
-        return cycleTime;
-    }
-
-    public void setCycleTime(double cycleTime) {
-        this.cycleTime = cycleTime;
-    }
-
-    public Boolean getPlaysDefense() {
-        return playsDefense;
-    }
-
-    public void setPlaysDefense(Boolean playsDefense) {
-        this.playsDefense = playsDefense;
-    }
-
-
-    public Boolean getAutoEndInCourtyard() {
-        return autoEndInCourtyard;
-    }
-
-    public void setAutoEndInCourtyard(Boolean autoEndInCourtyard) {
-        this.autoEndInCourtyard = autoEndInCourtyard;
-    }
-
-    public Boolean getAutoEndInNeutralZone() {
-        return autoEndInNeutralZone;
-    }
-
-    public void setAutoEndInNeutralZone(Boolean autoEndInNeutralZone) {
-        this.autoEndInNeutralZone = autoEndInNeutralZone;
-    }
-
-    public Boolean getCanScaleAtCenter() {
-        return canScaleAtCenter;
-    }
-
-    public void setCanScaleAtCenter(Boolean canScaleAtCenter) {
-        this.canScaleAtCenter = canScaleAtCenter;
-    }
-
-    public Boolean getCanScaleOnRight() {
-        return canScaleOnRight;
-    }
-
-    public void setCanScaleOnRight(Boolean canScaleOnRight) {
-        this.canScaleOnRight = canScaleOnRight;
-    }
-
-    public Boolean getCanScaleOnLeft() {
-        return canScaleOnLeft;
-    }
-
-    public void setCanScaleOnLeft(Boolean canScaleOnLeft) {
-        this.canScaleOnLeft = canScaleOnLeft;
-    }
-
-    public double getScaleHeightPercent() {
-        return scaleHeightPercent;
-    }
-
-    public void setScaleHeightPercent(double scaleHeightPercent) {
-        this.scaleHeightPercent = scaleHeightPercent;
     }
 
 }
