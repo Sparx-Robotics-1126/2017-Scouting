85df7ee7e560f33a613def6860eefcecf26b8d97
HIRAM E JIMENEZ <jimenez.hiram@gmail.com>
2017-02-21 17:12:43 -0500

    During View work

--- app/src/main/java/com/sparx1126/steamworks/ExpandableListDataPump.java
+++ app/src/main/java/com/sparx1126/steamworks/ExpandableListDataPump.java
@@ -1,36 +1,49 @@
 package com.sparx1126.steamworks;
 
 
+import android.util.Pair;
+
+import org.gosparx.scouting.aerialassist.dto.BenchmarkingData;
+import org.gosparx.scouting.aerialassist.dto.ScoutingInfo;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class ExpandableListDataPump {
     public static HashMap<String, List<String>> getData() {
-        HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
+        HashMap<String, List<String> > expandableListDetail = new HashMap<String, List<String>>();
+
+        ScoutingInfo displayedInfo = ScoutingInfo.getCurrentInfo();
+        BenchmarkingData benchmarkingData = displayedInfo.getBenchmarkingData();
 
         List<String> drives = new ArrayList<String>();
-        drives.add("Drive System:");
-        drives.add("Speed: " + "m/s");
-        drives.add("Can play defense:");
-
+        drives.add("Drive System: " + benchmarkingData.getDriveSystem());
+        drives.add("Speed: " + benchmarkingData.getDrivesSpeed() + "m/s");
+        drives.add("Can play defense: " + benchmarkingData.isCanPlayDefenseBenchButton());
         List<String> shooting = new ArrayList<String>();
-        shooting.add("Balls: ");
-        shooting.add("Balls per second: ");
-        shooting.add("Balls in one cycle: ");
-        shooting.add("Cycle time: ");
-        shooting.add("Shooting range: ");
-        shooting.add("Preferred shooting place: ");
-        shooting.add("Shooting accuracy: " + "%");
-        shooting.add("Can get balls from: ");
-        shooting.add("Prefers to get balls from: ");
-        shooting.add("can hold: " + "balls");
+        shooting.add("Can Shoot High: " + benchmarkingData.isAbilityToShootHighGoalBenchButton());
+        shooting.add("Ability to shoot low: " + benchmarkingData.isAbilityToShootLowGoalBenchButton());
+        shooting.add("Type of shooter: " + benchmarkingData.getTypeOfShooterBenchInput());
+        shooting.add("Balls per second: " + benchmarkingData.getBallsPerSecondBenchInput());
+        shooting.add("Balls per second: " + benchmarkingData.getBallsPerSecondBenchInput());
+        shooting.add("Balls in one cycle: " + benchmarkingData.getBallsInCycleBenchInput());
+        shooting.add("Cycle time High: " + benchmarkingData.getCycleTimeHighBenchInput());
+        shooting.add("Shooting range: " + benchmarkingData.getShootingRangeBenchInput());
+        shooting.add("Preferred shooting place: " + benchmarkingData.getPreferredShootingLocationBenchInput());
+        shooting.add("High Goal Shooting accuracy: " + benchmarkingData.getAccuracyHighBenchInput() + "%");
+        shooting.add("Can get balls from Hopper: "  + benchmarkingData.isPickupBallHopperBenchButton());
+        shooting.add("Can get balls from Floor: "  + benchmarkingData.isPickupBallFloorBenchButton());
+        shooting.add("Can get balls from Human: "  + benchmarkingData.isPickupBallHumanBenchButton());
+        shooting.add("Prefers to get balls from: " + benchmarkingData.getPickupBallPreferredBenchInput());
+        shooting.add("can hold: " + benchmarkingData.getMaximumBallCapacityBenchInput() + "balls");
 
         List<String> gears = new ArrayList<String>();
-        gears.add("Gears: ");
-        gears.add("can get gears from: ");
-        gears.add("Can score from: ");
-        gears.add("Prefers to score from: ");
+        gears.add("Can Score Gears: ");
+        gears.add("can get gears from floor: ");
+        gears.add("can get gears from retrieval: ");
+        gears.add("Prefers to get gears from retrieval");
+        gears.add("Can score gears left: " + benchmarkingData.isCanGearLeftBench());
         gears.add("Cycle time: ");
 
         List<String> lowGoal = new ArrayList<String>();
@@ -47,7 +60,7 @@ public class ExpandableListDataPump {
         auto.add("can do: ");
 
         List<String> otherComments = new ArrayList<String>();
-        otherComments.add("COOMOMOMMMMENTS");
+        otherComments.add("Comments");
 
 
 
