980f02b46f9817176226c289855fda624fe53ffc
Amanda <arlonghenry@gmail.com>
2017-02-21 20:40:39 -0500

    scoutingdata.java and benchmarkingdata.java SHOULD be finished

--- app/src/main/java/com/sparx1126/steamworks/ExpandableListDataPump.java
+++ app/src/main/java/com/sparx1126/steamworks/ExpandableListDataPump.java
@@ -29,42 +29,77 @@ public class ExpandableListDataPump {
         shooting.add("<font color=\"black\"><b>Can Shoot High: </b></font>" + benchmarkingData.isAbilityToShootHighGoalBenchButton());
         shooting.add("<font color=\"black\"><b>Ability to shoot low: </b></font>" + benchmarkingData.isAbilityToShootLowGoalBenchButton());
         shooting.add("<font color=\"black\"><b>Type of shooter: </b></font>" + benchmarkingData.getTypeOfShooterBenchInput());
-        shooting.add("<font color=\"black\"><b>Balls per second: </b></font>" + benchmarkingData.getBallsPerSecondBenchInput());
-        shooting.add("<font color=\"black\"><b><Balls per second: /b></font>" + benchmarkingData.getBallsPerSecondBenchInput());
-        shooting.add("<font color=\"black\"><b>Balls in one cycle: </b></font>" + benchmarkingData.getBallsInCycleBenchInput());
+        String ballsPerSecond = "";
+        if(benchmarkingData.getBallsPerSecondBenchInput() != Double.MAX_VALUE) {
+            ballsPerSecond = String.valueOf(benchmarkingData.getBallsPerSecondBenchInput());
+        }
+        shooting.add("<font color=\"black\"><b>Balls per second: </b></font>" + ballsPerSecond);
+        String ballsInCycleInput = "";
+        if(benchmarkingData.getBallsInCycleBenchInput() != Integer.MAX_VALUE) {
+            ballsInCycleInput = String.valueOf(benchmarkingData.getBallsInCycleBenchInput());
+        }
+        shooting.add("<font color=\"black\"><b>Balls in one cycle: </b></font>" + ballsInCycleInput);
+        String cycleTimeHighBenchInput = "";
+        if(benchmarkingData.getCycleTimeHighBenchInput() != Integer.MAX_VALUE) {
+            cycleTimeHighBenchInput = String.valueOf(benchmarkingData.getCycleTimeHighBenchInput());
+        }
         shooting.add("<font color=\"black\"><b>Cycle time High: </b></font>" + benchmarkingData.getCycleTimeHighBenchInput());
+        String shootingRangeBenchInput ="";
+        if(benchmarkingData.getShootingRangeBenchInput() != Integer.MAX_VALUE) {
+            shootingRangeBenchInput = String.valueOf(benchmarkingData.getShootingRangeBenchInput());
+        }
         shooting.add("<font color=\"black\"><b>Shooting range: </b></font>" + benchmarkingData.getShootingRangeBenchInput());
         shooting.add("<font color=\"black\"><b>Preferred shooting place: </b></font>" + benchmarkingData.getPreferredShootingLocationBenchInput());
+        String accuracyHighBenchInput ="";
+        if(benchmarkingData.getAccuracyHighBenchInput() != Double.MAX_VALUE) {
+            accuracyHighBenchInput = String.valueOf(benchmarkingData.getAccuracyHighBenchInput());
+        }
         shooting.add("<font color=\"black\"><b>High Goal  accuracy: </b></font>" + benchmarkingData.getAccuracyHighBenchInput() + "%");
         shooting.add("<font color=\"black\"><b>Can get balls from Hopper: </b></font>"  + benchmarkingData.isPickupBallHopperBenchButton());
         shooting.add("<font color=\"black\"><b>Can get balls from Floor: </b></font>"  + benchmarkingData.isPickupBallFloorBenchButton());
         shooting.add("<font color=\"black\"><b>Can get balls from Human: </b></font>"  + benchmarkingData.isPickupBallHumanBenchButton());
         shooting.add("<font color=\"black\"><b>Prefers to get balls from: </b></font>" + benchmarkingData.getPickupBallPreferredBenchInput());
+        String maximumBallCapacityBenchInput="";
+        if(benchmarkingData.getMaximumBallCapacityBenchInput() != Integer.MAX_VALUE) {
+            maximumBallCapacityBenchInput = String.valueOf(benchmarkingData.getMaximumBallCapacityBenchInput());
+        }
         shooting.add("<font color=\"black\"><b>can hold: </b></font>" + benchmarkingData.getMaximumBallCapacityBenchInput() + "balls");
 
         List<String> gears = new ArrayList<String>();
-        gears.add("Can Score Gears: ");
-        gears.add("can get gears from floor: ");
-        gears.add("can get gears from retrieval: ");
-        gears.add("Prefers to get gears from retrieval");
-        gears.add("Can score gears left: " + benchmarkingData.isCanGearLeftBench());
-        gears.add("Cycle time: ");
+        gears.add("<font color=\"black\"><b>Can Score Gears: </b></font>"  + benchmarkingData.isCanScoreGearsBenchButton());
+        gears.add("<font color=\"black\"><b>Can get gears from floor: </b></font>"  + benchmarkingData.isPickupGearFloorBenchButton());
+        gears.add("<font color=\"black\"><b>Can get gears from retrieval: </b></font>" + benchmarkingData.isPickupGearRetrievalBenchButton());
+        gears.add("<font color=\"black\"><b>Prefers to get gears from retrieval: </b></font>"  + benchmarkingData.isPickupGearPreferredBenchButton());
+        gears.add("<font color=\"black\"><b>Can score gears left: </b></font>" + benchmarkingData.isCanGearLeftBench());
+        String cycleTimeGearsBenchInput="";
+        if(benchmarkingData.getCycleTimeGearsBenchInput() != Integer.MAX_VALUE) {
+            cycleTimeGearsBenchInput = String.valueOf(benchmarkingData.getCycleTimeGearsBenchInput());
+        }
+        gears.add("<font color=\"black\"><b>Cycle time: </b></font>"  + benchmarkingData.getCycleTimeGearsBenchInput());
 
         List<String> lowGoal = new ArrayList<String>();
-        lowGoal.add("Can low goal: ");
-        lowGoal.add("Cycle time: ");
-        lowGoal.add("Number cycles: ");
+        lowGoal.add("<font color=\"black\"><b>Can low goal: </b></font>"  + benchmarkingData.isAbilityToShootLowGoalBenchButton());
+        String cycleTimeLowBenchInput="";
+        if(benchmarkingData.getCycleTimeLowBenchInput() != Integer.MAX_VALUE) {
+            cycleTimeLowBenchInput = String.valueOf(benchmarkingData.getCycleTimeLowBenchInput());
+        }
+        lowGoal.add("<font color=\"black\"><b>Cycle time: </b></font>"  + benchmarkingData.getCycleTimeLowBenchInput());
+        String cycleNumberLowBenchInput="";
+        if(benchmarkingData.getCycleNumberLowBenchInput() != Integer.MAX_VALUE) {
+            cycleNumberLowBenchInput = String.valueOf(benchmarkingData.getCycleNumberLowBenchInput());
+        }
+        lowGoal.add("<font color=\"black\"><b>Number of cycles: </b></font>" + benchmarkingData.getCycleNumberLowBenchInput());
 
         List<String> scaling = new ArrayList<String>();
-        scaling.add("Can scaling: ");
-        scaling.add("Can scale from: ");
-        scaling.add("Prefers to scale from: ");
+        scaling.add("<font color=\"black\"><b>Can scaling: </b></font>" + benchmarkingData.isAbilityScaleBenchButton());
+        scaling.add("<font color=\"black\"><b>Places they can scale from: " + benchmarkingData.getPlacesCanScaleBenchInput());
+        scaling.add("<font color=\"black\"><b>Prefers to scale from: </b></font>" + benchmarkingData.getPreferredPlacesScaleInput());
 
         List<String> auto = new ArrayList<String>();
-        auto.add("can do: ");
+        auto.add("<font color=\"black\"><b>can do: </b></font>" + benchmarkingData.getAutoAbilitiesBench());
 
         List<String> otherComments = new ArrayList<String>();
-        otherComments.add("Comments");
+        otherComments.add("<font color=\"black\"><b>Comments: </b></font>" + benchmarkingData.getCommentsBench());
 
 
 
